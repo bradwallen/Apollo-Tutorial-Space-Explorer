2428e1b5d46a4dd16accfadb97c5e0347b13ef25
Brad Allen <ballen@inseq-lab.com>
2020-01-30 15:20:50 -0600

    saving so I can go home

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,6 +1,7 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const { createStore } = require('./utils');
+const resolvers = require('./resolvers');
 
 const LaunchAPI = require('./datasources/launch');
 const UserAPI = require('./datasources/user');
@@ -9,6 +10,7 @@ const store = createStore();
 
 const server = new ApolloServer({ 
   typeDefs,
+  resolvers,
   dataSources: () => ({
     launchAPI: new LaunchAPI(),
     userAPI: new UserAPI({ store })
@@ -16,5 +18,5 @@ const server = new ApolloServer({
 });
 
 server.listen().then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
+    console.log(`🚀 Server is rockin and ready at ${url}`);
   });
